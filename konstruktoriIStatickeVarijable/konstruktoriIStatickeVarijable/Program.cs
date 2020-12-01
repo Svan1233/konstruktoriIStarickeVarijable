e61fb4ce7999377f12250c99192d3e604fd3e584
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-12-01 10:40:58 +0100

    Add files via upload

---
+++ konstruktoriIStatickeVarijable/konstruktoriIStatickeVarijable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktoriIStatickeVarijable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            PrvaKlasa y = new PrvaKlasa();
+            PrvaKlasa z = new PrvaKlasa();
+            Console.WriteLine(PrvaKlasa.Counter);
+            Console.ReadKey();
+            ToString();
+        }
+
+    }
+}
---
+++ konstruktoriIStatickeVarijable/konstruktoriIStatickeVarijable/PrvaKlasa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktoriIStatickeVarijable
+{
+    class PrvaKlasa
+    {
+        static int counter;
+        string ime;
+        string prezime;
+        
+        void ToString()
+        {
+            Console.WriteLine("Redni broj: " + counter + "," + ime + prezime);
+            
+        }
+        
+        public PrvaKlasa()
+        {
+            counter++;
+        }
+
+        public static int Counter { get => counter;}
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+    }
+}
